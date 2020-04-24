caa87f98108075d91c67b34ee2ea30903645223d
Igor Rozum <irozum@Igors-MacBook-Pro.local>
2020-04-24 11:25:39 -0400

    links adjusted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 /************************************************************************ *********
-* WEB422 – Assignment 05
+* WEB422 – Assignment 06
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
 * assignment has been copied manually or electronically from any other source (including web sites) or
 * distributed to other students. *
-* Name: Igor Rozum Student ID: 157823170 Date: 07/03/2020
-*
-********************************************************************************/
+* Name: Igor Rozum Student ID: 157823170 Date: 01/04/2020 *
+* Online Link: https://app--blog.herokuapp.com or https://irozum.ggswpdev.com/blog
+* ********************************************************************************/
 
 import { Component } from '@angular/core';
 
